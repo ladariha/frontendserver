d232fed96dc61da59a854b61066d49e8054f93e5
Lada Riha <riha.vladimir@gmail.com>
2017-11-03 13:07:25 +0100

    do not compress SSE

--- src/compression/compression.js
+++ src/compression/compression.js
@@ -3,6 +3,13 @@
 const compression = require("compression");
 
 exports.init = (server, app, logger, {level = 6, threshold = "10kb"} = {}) => {
-    app.use(compression({level, threshold}));
+    app.use(compression(
+        {
+            level: level,
+            threshold: threshold,
+            filter: (req, res) => compression.filter(req, res) && res.getHeader("Content-Type") !== "text/event-stream"
+        }
+    ))
+    ;
     return Promise.resolve(app);
 };
